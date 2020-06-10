3c9ef2bb585b935f7885a0743e36e092fef6a6d2
Caleb Hatcher <caleb.hatcher@shockoe.com>
2020-06-10 11:55:21 -0400

    Init.

---
+++ lib/HeaderModal.d.ts
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { ImageSourcePropType, StyleProp, ViewStyle, ImageStyle, TextStyle } from 'react-native';
+interface HeaderModalProps {
+    withFilter?: boolean;
+    withCloseButton?: boolean;
+    closeButtonImage?: ImageSourcePropType;
+    closeButtonStyle?: StyleProp<ViewStyle>;
+    closeButtonImageStyle?: StyleProp<ImageStyle>;
+    headerTitle?: string;
+    headerStyle: StyleProp<TextStyle>;
+    onClose(): void;
+    renderFilter(props: HeaderModalProps): ReactNode;
+}
+export declare const HeaderModal: {
+    (props: HeaderModalProps): JSX.Element;
+    defaultProps: {
+        withCloseButton: boolean;
+    };
+};
+export {};
---
+++ src/HeaderModal.tsx
@@ -0,0 +1,59 @@
+import React, { ReactNode } from 'react'
+import {
+  View,
+  StyleSheet,
+  ImageSourcePropType,
+  StyleProp,
+  ViewStyle,
+  ImageStyle,
+  TextStyle,
+  Text,
+} from 'react-native'
+import CloseButton from './CloseButton'
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+	alignItems: 'center',
+	justifyContent: 'space-between'
+  }
+})
+
+interface HeaderModalProps {
+  withFilter?: boolean
+  withCloseButton?: boolean
+  closeButtonImage?: ImageSourcePropType
+  closeButtonStyle?: StyleProp<ViewStyle>
+  closeButtonImageStyle?: StyleProp<ImageStyle>
+  headerTitle?: string
+  headerStyle: StyleProp<TextStyle>
+  onClose(): void
+  renderFilter(props: HeaderModalProps): ReactNode
+}
+export const HeaderModal = (props: HeaderModalProps) => {
+  const {
+    withCloseButton,
+    closeButtonImage,
+    closeButtonStyle,
+	closeButtonImageStyle,
+	headerTitle,
+	headerStyle,
+    onClose,
+  } = props
+  return (
+    <View style={styles.container}>
+      {withCloseButton && <CloseButton
+        image={closeButtonImage}
+        style={closeButtonStyle}
+        imageStyle={closeButtonImageStyle}
+        onPress={onClose}
+      />}
+      <Text>{headerTitle}</Text>
+	  <View style={headerStyle}>{headerTitle}</View>
+    </View>
+  )
+}
+
+HeaderModal.defaultProps = {
+  withCloseButton: true
+}
